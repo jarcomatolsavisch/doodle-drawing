d7c2e7eb53ff4b6ed351451e875d291fc8c2df7a
jarcomatolsavisch <ma6036tp@gmail.com>
2021-07-26 01:05:58 +0800

    correct main.js

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-/*
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8888;
-app.get("/", (req, res) => {
-    res.end("Hello World, Welcome.");
-});
-
-app.get("/test", (req, res) => {
-    res.end("You are in the /test");
-});
-
-app.listen(port, () => {
-    console.log(`server listen on port =${port}`)
-});
-*/
-
 const express = require('express');
 const path = require('path');
 const app = express();
