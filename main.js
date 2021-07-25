c8b093059144625671c6d48b275776ed10f20364
jarcomatolsavisch <ma6036tp@gmail.com>
2021-07-26 00:15:50 +0800

    test

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 const express = require('express');
+const app = express();
+const port = process.env.PORT || 8888;
+app.get("/", (req, res) => {
+    res.end("Hello World, Welcome.");
+});
+
+app.get("/test", (req, res) => {
+    res.end("You are in the /test");
+});
+
+app.listen(port, () => {
+    console.log(`server listen on port =${port}`)
+});
+/*
+const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static('./public'));
@@ -14,3 +29,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`server listen on port =${port}`)
 });
+*/
